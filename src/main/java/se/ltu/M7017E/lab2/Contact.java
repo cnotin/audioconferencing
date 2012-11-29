06ec395c21873ac33c96aa96af3949ff3811ed70
Hervé Loeffel <herloe-2@A3402.its.ltu.se>
2012-11-29 14:00:02 +0100

    ip

--- src/main/java/se/ltu/M7017E/lab2/Contact.java
+++ src/main/java/se/ltu/M7017E/lab2/Contact.java
@@ -3,6 +3,7 @@ package se.ltu.M7017E.lab2;
 public class Contact {
 
 	private String name;
+	private String ip;
 
 	public Contact(String name, String ip) {
 		this.name = name;
@@ -20,4 +21,13 @@ public class Contact {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
 }
