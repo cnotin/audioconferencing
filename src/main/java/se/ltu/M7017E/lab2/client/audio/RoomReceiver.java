8ff7291609acb1deeeff0b1fde8460fa0088f47e
ClementNotin <clement@notin.org>
2012-11-29 18:57:17 +0100

    Create the RoomReceiver bin and add the needed RTP decode bin from the repo gst-rtp-experiments

---
+++ src/main/java/se/ltu/M7017E/lab2/client/audio/RoomReceiver.java
@@ -0,0 +1,85 @@
+package se.ltu.M7017E.lab2.client.audio;
+
+import org.gstreamer.Bin;
+import org.gstreamer.Caps;
+import org.gstreamer.Element;
+import org.gstreamer.ElementFactory;
+import org.gstreamer.GhostPad;
+import org.gstreamer.Pad;
+import org.gstreamer.PadLinkReturn;
+
+import se.ltu.M7017E.lab2.client.Tool;
+
+public class RoomReceiver extends Bin {
+	private final Element udpSource;
+	private final Element rtpBin;
+	private final Element adder;
+	private final Pad src;
+
+	public RoomReceiver(String ip, int port) {
+		super();
+
+		udpSource = ElementFactory.make("udpsrc", null);
+		udpSource.set("multicast-group", ip);
+		udpSource.set("port", port);
+		udpSource.set("auto-multicast", true);
+
+		Tool.successOrDie("caps",
+				udpSource.getStaticPad("src").setCaps(
+						Caps.fromString("application/x-rtp, media=audio, "
+								+ "clock-rate=8000, channel=1, payload=0, "
+								+ "encoding-name=PCMU")));
+
+		rtpBin = ElementFactory.make("gstrtpbin", null);
+		adder = ElementFactory.make("liveadder", null);
+
+		// ####################### CONNECT EVENTS ######################"
+		rtpBin.connect(new Element.PAD_ADDED() {
+			@Override
+			public void padAdded(Element element, Pad pad) {
+				System.out.println("Pad added: " + pad);
+				if (pad.getName().startsWith("recv_rtp_src")) {
+					/*
+					 * TODO : if SSRC=myself.ssrc then connect to fakesink to
+					 * prevent echo of my own voice
+					 */
+
+					System.out.println("\nGot new sound input pad: " + pad);
+
+					// create elements
+					RtpMulawDecodeBin decoder = new RtpMulawDecodeBin();
+
+					// add them
+					RoomReceiver.this.add(decoder);
+
+					// sync them
+					decoder.syncStateWithParent();
+
+					// link them
+					Tool.successOrDie(
+							"bin-decoder",
+							pad.link(decoder.getStaticPad("sink")).equals(
+									PadLinkReturn.OK));
+
+					Pad adderPad = adder.getRequestPad("sink%d");
+					Tool.successOrDie(
+							"decoder-adder",
+							decoder.getStaticPad("src").link(adderPad)
+									.equals(PadLinkReturn.OK));
+				}
+			}
+		});
+
+		// ############## ADD THEM TO PIPELINE ####################
+		addMany(udpSource, rtpBin, adder);
+
+		// Now they are in the pipeline, we can add the ghost pad
+		src = new GhostPad("src", adder.getStaticPad("src"));
+		addPad(src);
+
+		// ###################### LINK THEM ##########################
+		Pad pad = rtpBin.getRequestPad("recv_rtp_sink_0");
+		Tool.successOrDie("udpSource-rtpbin", udpSource.getStaticPad("src")
+				.link(pad).equals(PadLinkReturn.OK));
+	}
+}
