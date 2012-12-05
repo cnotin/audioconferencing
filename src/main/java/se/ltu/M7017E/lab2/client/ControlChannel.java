19821e87c56e67bdc2166abe812e87084dc90dd7
ClementNotin <clement@notin.org>
2012-12-05 21:29:46 +0100

    Use Herve's IP

--- src/main/java/se/ltu/M7017E/lab2/client/ControlChannel.java
+++ src/main/java/se/ltu/M7017E/lab2/client/ControlChannel.java
@@ -39,7 +39,8 @@ public class ControlChannel implements Runnable {
 		this.app = app;
 
 		try {
-			Socket socket = new Socket(InetAddress.getByName("127.0.0.1"), 4000);
+			Socket socket = new Socket(InetAddress.getByName("130.240.53.166"),
+					4000);
 			in = new BufferedReader(new InputStreamReader(
 					socket.getInputStream()));
 			out = new PrintStream(socket.getOutputStream());
